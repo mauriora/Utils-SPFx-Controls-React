54aa841f1151b29fe71a1d8405b38d76dec758cd
denniskuhn <dennis@kuhn.geek.nz>
2021-10-15 14:15:58 +1300

    Errorboundary ignoredExceptions through props

--- src/components/ErrorBoundary.tsx
+++ src/components/ErrorBoundary.tsx
@@ -1,9 +1,11 @@
 import * as React from "react";
-import { ErrorBar } from '..'
+import { GetDerivedStateFromError, GetDerivedStateFromProps } from "react";
+import { ErrorBar } from '../components/MessageBar';
 
 interface ErrorBoundaryState {
     error: undefined | unknown;
     info: undefined | ErrorInfo;
+    ignoredExceptions: Array<string>;
 }
 
 interface ODataError {
@@ -28,20 +30,31 @@ interface ErrorVariants {
 
 }
 
-export class ErrorBoundary extends React.Component<unknown, ErrorBoundaryState> {
-    constructor(props: unknown) {
-        super(props);
-        this.state = { error: undefined, info: undefined };
-    }
+export interface ErrorBoundaryProps {
+    ignoredExceptions?: Array<string>;
+}
 
-    public ignoredExceptions = ['Cannot access https://webshell.suite.office.com', 'Failed to retrieve a valid token'];
+export class ErrorBoundary extends React.Component<ErrorBoundaryProps, ErrorBoundaryState> {
+    private static defaultIgnoredExceptions = ['Cannot access https://webshell.suite.office.com', 'Failed to retrieve a valid token']
+
+    constructor(props: ErrorBoundaryProps) {
+        super(props);
+        const ignoredExceptions = ErrorBoundary.defaultIgnoredExceptions;
+
+        if(props.ignoredExceptions && props.ignoredExceptions.length) {
+            ignoredExceptions.push( ... props.ignoredExceptions );
+        }
+        this.state = { error: undefined, info: undefined, ignoredExceptions };
+
+        console.log(`ErrorBoundary()`, {ignoredExceptions, props: this.props, state: this.state, defaultIgnoredExceptions: ErrorBoundary.defaultIgnoredExceptions});
+    }
 
     private promiseRejectionHandler = (event: PromiseRejectionEvent) => {
 
-        if (this.ignoredExceptions.includes( event?.reason?.message )) {
-            console.error(`ErrorBoundary.promiseRejectionHandler(): ignore: ${event.reason.message}`, {event, reason: event.reason});
+        if (this.state.ignoredExceptions.includes( event?.reason?.message )) {
+            console.error(`ErrorBoundary.promiseRejectionHandler(): ignore: '${event.reason.message}'`, {event, reason: event.reason, state: this.state});
         } else {
-            console.error(`ErrorBoundary.promiseRejectionHandler( ${event?.reason?.message})`, { event });
+            console.error(`ErrorBoundary.promiseRejectionHandler( ${event?.reason?.message} )`, { event });
             this.setState({
                 error: event.reason ?? event.type,
                 info: { title: event.reason?.message ?? event.type }
@@ -70,7 +83,7 @@ export class ErrorBoundary extends React.Component<unknown, ErrorBoundaryState> 
     }
 
     /** Return new state so the next render will show the fallback UI.  */
-    static getDerivedStateFromError = (error: ErrorVariants): ErrorBoundaryState => ({ error, info: ErrorBoundary.parseError(error) });
+    static getDerivedStateFromError: GetDerivedStateFromError<ErrorBoundaryProps, ErrorBoundaryState> = (error: ErrorVariants) => ({ error, info: ErrorBoundary.parseError(error) });
 
     componentDidCatch(error: unknown, errorInfo: unknown): void {
         // You can also log the error to an error reporting service
@@ -85,7 +98,28 @@ export class ErrorBoundary extends React.Component<unknown, ErrorBoundaryState> 
         window.removeEventListener('unhandledrejection', this.promiseRejectionHandler);
     }
 
+    static getDerivedStateFromProps: GetDerivedStateFromProps<ErrorBoundaryProps, ErrorBoundaryState> = (nextProps, prevState) => {
+        const prevCustomIgnored = prevState.ignoredExceptions.slice(ErrorBoundary.defaultIgnoredExceptions.length);
+        const changed = prevCustomIgnored.some( (prevIgnored, index) => prevIgnored !== nextProps.ignoredExceptions?.[index] );
+
+        if(changed) {
+            const ignoredExceptions = ErrorBoundary.defaultIgnoredExceptions;
+
+            if(nextProps.ignoredExceptions && nextProps.ignoredExceptions.length) {
+                ignoredExceptions.push( ... nextProps.ignoredExceptions );
+            }
+            console.log(`ErrorBoundary.getDerivedStateFromProps changed`, {nextProps, prevState, prevCustomIgnored, ignoredExceptions });
+    
+            return { ignoredExceptions };
+        }
+        console.log(`ErrorBoundary.getDerivedStateFromProps not changed`, {nextProps, prevState, prevCustomIgnored });
+
+        return null;
+    }
+
     render(): React.ReactNode {
+        console.log(`ErrorBoundary.render()`, {state: this.state, props: this.props, me: this});
+
         if (this.state.error) {
             return <ErrorBar
                 message={`${String(this.state.info.title ?? this.state.error + (this.state.info.description ? ': ' + this.state.info.description : ''))}`}
