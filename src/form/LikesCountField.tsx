e70c3776b59303e3753742ef8faaa3a19af35ce2
denniskuhn <dennis@kuhn.geek.nz>
2021-11-07 13:07:30 +1300

    Chore[LikeCountField]: more ootb: heart, wording

--- src/form/LikesCountField.tsx
+++ src/form/LikesCountField.tsx
@@ -1,37 +1,50 @@
 import * as React from 'react';
-import { FunctionComponent } from 'react';
+import { FunctionComponent, useMemo } from 'react';
 import { observer } from 'mobx-react-lite';
-import { ITextFieldStyles, TextField } from '@fluentui/react';
+import { IconButton, IIconProps, ITextFieldStyles, Stack, Text } from '@fluentui/react';
 import { PropertyFieldProps } from './PropertyField';
 
 export interface LikesCountFieldProps extends PropertyFieldProps {
     mini?: boolean;
 }
 
-const transparentFieldStyles: Partial<ITextFieldStyles> = { 
+const transparentFieldStyles: Partial<ITextFieldStyles> = {
     fieldGroup: {
         background: 'transparent',
         border: 'none'
     }
 };
 
-export const LikesCountField: FunctionComponent<LikesCountFieldProps> = observer(({ info, item, property, mini }) =>{
+export const LikesCountField: FunctionComponent<LikesCountFieldProps> = observer(({ info, item, property, mini }) => {
     const value = item[property];
-    if ( undefined !== value && null !== value && typeof value !== 'number') throw new Error(`Property '${property}' is not a number, null or undefined, but it's of type ${typeof value}:${String(value)}`);
 
-    return <TextField
-        iconProps={{
-            iconName: item.isLikedByMe() ? 'LikeSolid' : 'Like',
+    if (undefined !== value && null !== value && typeof value !== 'number') throw new Error(`Property '${property}' is not a number, null or undefined, but it's of type ${typeof value}:${String(value)}`);
+
+    const fieldWidthStyle = useMemo(() =>
+        mini ?
+            { minWidth: 40, width: 40 + ((Math.floor(Math.log10(item[property] ? Number(item[property]) : 1)) + 1) * 15) } :
+            undefined,
+        [value, mini]
+    );
+
+    const iconProps = useMemo<IIconProps>(
+        () => ({
+            iconName: item.isLikedByMe() ? 'HeartFill' : 'Heart',
             style: { pointerEvents: "auto", cursor: "pointer" },
             onClick: () => item.toggleLike()
-        }}
-        style={mini ? { minWidth: 40, width: 40 + ((Math.floor(Math.log10(item[property] ? Number(item[property]) : 1)) + 1) * 15) } : undefined}
-        styles={mini ? transparentFieldStyles : undefined}
-        label={mini ? undefined : info.Title}
-        value={value as unknown as string}
-        readOnly={true}
-        placeholder={info.Description}
-        title={info.Title}
-    />
+        }),
+        [item, item.isLikedByMe()]
+    );
+
+    return <Stack horizontal>
+        <IconButton iconProps={iconProps} />
+        <Text
+            style={fieldWidthStyle}
+            styles={mini ? transparentFieldStyles : undefined}
+            title={info.Title}
+        >
+            {`${value ?? 0} Like${value === 1 ? '' : 's'}`}
+        </Text>
+    </Stack>
 });
 
