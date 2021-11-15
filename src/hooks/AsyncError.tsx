f0b113c1689e487636de24d924241114e2e2c38a
denniskuhn <dennis@kuhn.geek.nz>
2021-11-15 18:14:39 +1300

    fix: remove invalid left over

--- src/hooks/AsyncError.tsx
+++ src/hooks/AsyncError.tsx
@@ -1,31 +1,5 @@
 import { useCallback, useState } from "react";
 
-.
-
-Example:
-
-```typescript
-    import { useAsyncError } from '@mauriora/utils-spfx-controls-react';
-
-    const ItemForm: FunctionComponent<...> = ({ model, item }) => {
-
-        const throwError = useAsyncError();
-
-        const submitItem = useCallback(
-            async () => {
-                try {
-                    await model.submit(item);
-                } catch (submitError) {
-                    throwError(submitError);
-                }
-            },
-            [model, item]
-        );
-        ...
-    }
-
-```
-
 /**
  * catch an error in an async operation and rethrow it in the main loop.
  * @example 
