fe1e212f71e38fa7962056ad0c8cc50dec0a8cbf
denniskuhn <dennis@kuhn.geek.nz>
2021-11-15 18:12:38 +1300

    chore: update comments

--- src/hooks/AsyncError.tsx
+++ src/hooks/AsyncError.tsx
@@ -1,5 +1,52 @@
 import { useCallback, useState } from "react";
 
+.
+
+Example:
+
+```typescript
+    import { useAsyncError } from '@mauriora/utils-spfx-controls-react';
+
+    const ItemForm: FunctionComponent<...> = ({ model, item }) => {
+
+        const throwError = useAsyncError();
+
+        const submitItem = useCallback(
+            async () => {
+                try {
+                    await model.submit(item);
+                } catch (submitError) {
+                    throwError(submitError);
+                }
+            },
+            [model, item]
+        );
+        ...
+    }
+
+```
+
+/**
+ * catch an error in an async operation and rethrow it in the main loop.
+ * @example 
+ * const ItemForm: FunctionComponent<...> = ({ model, item }) => {
+
+        const throwError = useAsyncError();
+
+        const submitItem = useCallback(
+            async () => {
+                try {
+                    await model.submit(item);
+                } catch (submitError) {
+                    throwError(submitError);
+                }
+            },
+            [model, item]
+        );
+        ...
+    }
+ * 
+ */
 export const useAsyncError = (): (e: unknown) => void => {
     const [, setError] = useState();
 
