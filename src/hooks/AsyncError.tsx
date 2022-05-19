30cefc4e651d0c86242b23d6ed42560002f1bed4
Dennis Kuhn <dennis@kuhn.geek.nz>
2022-05-20 07:08:11 +1200

    merging

--- src/hooks/AsyncError.tsx
+++ src/hooks/AsyncError.tsx
@@ -1,5 +1,26 @@
 import { useCallback, useState } from "react";
 
+/**
+ * catch an error in an async operation and rethrow it in the main loop.
+ * @example 
+ * const ItemForm: FunctionComponent<...> = ({ model, item }) => {
+
+        const throwError = useAsyncError();
+
+        const submitItem = useCallback(
+            async () => {
+                try {
+                    await model.submit(item);
+                } catch (submitError) {
+                    throwError(submitError);
+                }
+            },
+            [model, item]
+        );
+        ...
+    }
+ * 
+ */
 export const useAsyncError = (): (e: unknown) => void => {
     const [, setError] = useState();
 
